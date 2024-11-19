40f24dfd744825157270d855242dcc05abf0c729
jannabiforever <51292893+jannabiforever@users.noreply.github.com>
2024-11-19 19:03:05 +0900

    some nice crocodile picture right in the mid

---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer>
+    <p>Crocodiles are <strong>awesome</strong>.</p>
+</footer>
+
+<style>
+    footer {
+        background-color: rgba(245, 245, 245, 0.9);
+        padding: 1rem;
+        margin: 0;
+        text-align: center;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Header from "../components/Header.svelte";
+    import Footer from "../components/Footer.svelte";
+</script>
+
+<Header></Header>
+<div class="main-content">
+    <slot></slot>
+</div>
+<Footer></Footer>
+
+<style>
+    .main-content {
+        width: 60%;
+        min-height: 100vh;
+
+        display: block;
+        justify-self: center;
+
+        background-image: url("../../croco.png");
+        background-color: rgba(255, 255, 255, 0.9);
+        background-blend-mode: overlay;
+    }
+</style>
