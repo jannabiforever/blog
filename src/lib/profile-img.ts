33d05ac21b8cf0d0af34e496da2b20a0bbd11cfb
jannabiforever <51292893+jannabiforever@users.noreply.github.com>
2024-11-29 21:44:04 +0900

    slight changes on getProfileImagePaths: using alias

--- src/lib/profile-img.ts
+++ src/lib/profile-img.ts
@@ -1,4 +1,4 @@
 export function getProfileImagePaths(): string[] {
-	const modules = import.meta.glob('/src/lib/assets/profile_image/*');
+	const modules = import.meta.glob('$lib/assets/profile_image/*');
 	return Object.keys(modules);
 }
