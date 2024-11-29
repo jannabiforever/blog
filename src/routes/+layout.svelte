3216aedbfb41362d5c329e10f69b8191524fc3f6
jannabiforever <51292893+jannabiforever@users.noreply.github.com>
2024-11-29 18:19:32 +0900

    carbon setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
+	import 'carbon-components-svelte/css/all.css';
+	import { Link, OutboundLink } from 'carbon-components-svelte';
+	import { LogoInstagram, LogoGithub } from 'carbon-icons-svelte';
+
 	let { children } = $props();
 	const currentYear: number = new Date().getFullYear();
 </script>
 
 <header>
 	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/posts">posts</a></li>
-			<li><a href="/songs">Songs</a></li>
-		</ul>
+		<div class="nav-links">
+			<Link href="/">Home</Link>
+			<Link href="/about">About</Link>
+			<Link href="/posts">Posts</Link>
+			<Link href="/songs">Songs</Link>
+		</div>
+		<div class="toggle-theme">
+			<button>Toggle Theme</button>
+		</div>
 	</nav>
 </header>
 
@@ -19,11 +26,19 @@
 </main>
 
 <footer>
-	<div class="copyright-claim">&copy; {currentYear} Jungin Yu.</div>
+	<div class="copyright-claim">&copy; {currentYear} Jungin Yu. All rights reserved.</div>
 	<div class="social-links">
 		<div class="social-link">
-			<a href="http://www.instagram.com/4ortis_mane">instagram</a>
-			<a href="http://www.github.com/jannabiforever">github</a>
+			<OutboundLink href="http://www.instagram.com/4ortis_mane">
+				<LogoInstagram size={20} />
+				instagram
+			</OutboundLink>
+		</div>
+		<div class="social-link">
+			<OutboundLink href="http://www.github.com/jannabiforever">
+				<LogoGithub size={20} />
+				github
+			</OutboundLink>
 		</div>
 	</div>
 </footer>
@@ -36,13 +51,22 @@
 	}
 
 	header {
-		background-color: rgba(0, 0, 0, 0.1);
+		padding-top: 1vh;
 	}
 
-	header nav ul {
-		width: 50%;
-
+	nav {
 		display: flex;
 		justify-content: space-between;
+	}
+
+	.nav-links {
+		width: 50%;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.social-links {
+		display: flex;
+		flex-direction: column;
 	}
 </style>
