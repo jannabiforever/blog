ca853f2788cc6d77e6a6fb2f081612fb62fb9025
jannabiforever <jungini1226@gmail.com>
2025-04-27 15:23:28 +0900

    renamed post -> postcontainer

---
+++ src/lib/components/PostContainer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const { postMetadata, children }: { postMetadata: App.PostMetadata; children: Snippet } =
+		$props();
+</script>
+
+<h1 class="h1">{postMetadata.title}</h1>
+<div class="flex justify-between pt-2 pb-2">
+	<p class="opacity-60">{postMetadata.createdAt.toLocaleDateString()}</p>
+	<p class="opacity-60">{postMetadata.category}</p>
+</div>
+
+{#if children}
+	<article>
+		{@render children()}
+	</article>
+{/if}
