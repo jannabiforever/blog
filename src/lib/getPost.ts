4fc3a158aa71563d21c8af7b19b96d6349f70b94
jannabiforever <jungini1226@gmail.com>
2025-04-29 23:37:12 +0900

    first post completed

--- src/lib/getPost.ts
+++ src/lib/getPost.ts
@@ -12,8 +12,8 @@ export async function getAllPostMetadatum(): Promise<App.PostMetadata[]> {
 	return [
 		{
 			id: 1,
-			title: '검색 엔진 인덱싱 - MeiliSearch 탐구하기',
-			createdAt: new Date(),
+			title: '검색 엔진 - MeiliSearch 코드 베이스 탐구하기',
+			createdAt: new Date('2025-04-29'),
 			category: ['DB', 'Search Engine'],
 			imgSrc: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg'
 		}
